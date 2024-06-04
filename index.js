191731aa3893f62f4bc592feaddb0e1211a79b0b
unknown <honey25303@gmail.com>
2024-06-04 11:24:07 +0530

    updated path

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    console.log('Launching browser');
+    console.log("Launching browser");
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: process.env.CHROME_PATH || undefined, // Use the environment variable if set
+      headless: true,
+      executablePath: `/usr/bin/google-chrome`, // Use the environment variable if set
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
-    console.log('Opening new page');
+    console.log("Opening new page");
     const page = await browser.newPage();
 
     // Set the HTML content you want to convert to PDF
@@ -30,25 +31,25 @@ app.get('/', async (req, res) => {
       </html>
     `;
 
-    console.log('Setting HTML content');
+    console.log("Setting HTML content");
     await page.setContent(htmlContent);
 
-    console.log('Generating PDF');
-    const pdfBuffer = await page.pdf({ format: 'A4', timeout: 30000 });
+    console.log("Generating PDF");
+    const pdfBuffer = await page.pdf({ format: "A4", timeout: 30000 });
 
-    console.log('Closing browser');
+    console.log("Closing browser");
     await browser.close();
 
     // Set the header for the PDF download
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename=output.pdf');
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "inline; filename=output.pdf");
 
     // Send the PDF buffer as a response
-    console.log('Sending PDF response');
+    console.log("Sending PDF response");
     res.send(pdfBuffer);
   } catch (error) {
-    console.error('Error generating PDF:', error);
-    res.status(500).send('Error generating PDF!!! ' + error);
+    console.error("Error generating PDF:", error);
+    res.status(500).send("Error generating PDF!!! " + error);
   }
 });
 
